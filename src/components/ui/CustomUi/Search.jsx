1b9ca67c35af44efa71004d1b7c27fd8d8e2e1af
Abdullah-warraich-ch <abdullahnasar333@gmail.com>
2026-04-09 20:52:07 +0500

    Search COmponent

---
+++ src/components/ui/CustomUi/Search.jsx
@@ -0,0 +1,19 @@
+import { SearchIcon } from "lucide-react";
+import React from "react";
+
+function Search({ placeholder }) {
+  return (
+    <div className="relative w-full ">
+      <div className="text-secondary-text absolute left-4 top-3">
+        <SearchIcon size={15} />
+      </div>
+      <input
+        type="text"
+        className="bg-input text-secondary-text p-2 rounded-lg text-[14px] border border-transparent pl-10 w-full focus:border-button focus:outline-0"
+        placeholder={placeholder}
+      />
+    </div>
+  );
+}
+
+export default Search;
--- src/screens/Orders.jsx
+++ src/screens/Orders.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import Search from "@/components/ui/CustomUi/Search";
+import React from "react";
 
 function Orders() {
   return (
-    <div>Orders</div>
-  )
+    <div className=" p-8 flex flex-col gap-10">
+      <div>
+        <Search placeholder="Search Orders By OrderId, Customer Name, Email..." />
+      </div>
+    </div>
+  );
 }
 
-export default Orders
+export default Orders;
